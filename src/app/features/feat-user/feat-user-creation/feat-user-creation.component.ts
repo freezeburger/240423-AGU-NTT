eb04f8c1342cc2065f5adb03f742fb1f1e6a23cd
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-04-30 17:51:44 +0200

    Update

--- src/app/features/feat-user/feat-user-creation/feat-user-creation.component.ts
+++ src/app/features/feat-user/feat-user-creation/feat-user-creation.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feat-user-creation',
-  templateUrl: './feat-user-creation.component.html',
-  styleUrl: './feat-user-creation.component.scss'
+  templateUrl: './feat-user-creation.component.html'
 })
 export class FeatUserCreationComponent {
 
